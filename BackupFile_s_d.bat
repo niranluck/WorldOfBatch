d2226cffb9f1c9af09b9bb47a0c31d9861d83ee0
niranluck <niran.l@outlook.com>
2024-06-26 11:43:43 +0700

    Check empty file
    Check empty source file before copy to destination file for backup

--- BackupFile_s_d.bat
+++ BackupFile_s_d.bat
@@ -1,7 +1,7 @@
 @echo off
 REM Check if exactly two arguments are provided
 if "%~2"=="" (
-    echo Usage: copyfile.bat source_file destination_file
+    echo Usage: BackupFile_s_d.bat source_file destination_file
     exit /b 1
 )
 
@@ -15,16 +15,18 @@ if not exist "%source_file%" (
     exit /b 1
 )
 
-REM Check if destination file exists and delete if it does
-if exist "%destination_file%" (
+REM Check file size not equal 0
+for %%A in ("%source_file%") do if %%~zA NEQ 0 (
+
+  REM Check if destination file exists and delete if it does
+  if exist "%destination_file%" (
     del "%destination_file%"
     if %errorlevel% neq 0 (
         echo Failed to delete existing destination file: %destination_file%
         exit /b 1
     )
-)
-REM Check file size not equal 0
-for %%A in ("%source_file%") do if %%~zA NEQ 0 (
+  )
+
   REM Copy the file
   copy "%source_file%" "%destination_file%"
 )
